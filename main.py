6731d3360b82ec190e65b48287bf976607be670d
Muhammadqodir1275 <muhammadqodir1275@gmail.com>
2025-10-19 21:20:13 +0500

    first commit

---
+++ main.py
@@ -0,0 +1,38 @@
+import telebot
+
+# Bot tokeni (BotFather’dan olingan tokenni shu joyga yozing)
+TOKEN = "8442519283:AAEzfPoFjIoBrtuI3uQhBYuAfYgrt8IRfxw"
+
+bot = telebot.TeleBot(TOKEN)
+
+# /start komandasi
+@bot.message_handler(commands=['start'])
+def send_welcome(message):
+    user = message.from_user.first_name  # foydalanuvchi ismi
+    text = f"👋 Salom, {user}!\n\n🤖 Men sizning yordamchingizman!\n\n"
+    text += "Quyidagi menyulardan birini tanlang 👇"
+
+    # Tugmalar
+    markup = telebot.types.ReplyKeyboardMarkup(resize_keyboard=True)
+    btn1 = telebot.types.KeyboardButton("ℹ️ Ma'lumot")
+    btn2 = telebot.types.KeyboardButton("🕹 Funksiya")
+    btn3 = telebot.types.KeyboardButton("💬 Aloqa")
+    markup.add(btn1, btn2, btn3)
+
+    bot.send_message(message.chat.id, text, reply_markup=markup)
+
+# Oddiy matnli xabarlar uchun
+@bot.message_handler(func=lambda message: True)
+def menu_handler(message):
+    if message.text == "ℹ️ Ma'lumot":
+        bot.reply_to(message, "Bu bot test loyihasi. Siz undan turli funksiya o‘rganishingiz mumkin 😊")
+    elif message.text == "🕹 Funksiya":
+        bot.reply_to(message, "Hozircha bu bo‘limda yangi funksiyalar sinovda 👨‍💻")
+    elif message.text == "💬 Aloqa":
+        bot.reply_to(message, "Admin bilan aloqa: @YourUsername")
+    else:
+        bot.reply_to(message, "Iltimos, menyudagi tugmalardan birini tanlang 👇")
+
+# Botni ishga tushurish
+print("🤖 Bot ishga tushdi...")
+bot.polling(non_stop=True)
